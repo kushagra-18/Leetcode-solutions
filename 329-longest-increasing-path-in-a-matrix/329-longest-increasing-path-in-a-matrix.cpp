dcaee928e7055602650b69a2e067fcf243ef3c24
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-05-20 00:25:20 +0530

    Time: 40 ms (95.96%), Space: 16.1 MB (55.62%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
-int dp[200][200]{}; // constraints are small enough that we can just set them to MAX
-int maxPath, n, m;
-int longestIncreasingPath(vector<vector<int>>& matrix) {
-	maxPath = 0, n = size(matrix), m = size(matrix[0]);
-	for(int i = 0; i < n; i++)
-		for(int j = 0; j < m; j++)
-			maxPath = max(maxPath, solve(matrix, i, j, -1));            
-	return maxPath;
-}
-int solve(vector<vector<int>>& mat, int i, int j, int prev){
-	if(i < 0 || j < 0 || i >= n || j >= m || mat[i][j] <= prev) return 0;
-	if(dp[i][j]) return dp[i][j];
-	return dp[i][j] = 1 + max({ solve(mat, i + 1, j, mat[i][j]),
-							    solve(mat, i - 1, j, mat[i][j]),
-							    solve(mat, i, j + 1, mat[i][j]),
-							    solve(mat, i, j - 1, mat[i][j]) });       
-}
-
+    int helper(vector<vector<int>>& matrix,int n,int m,int i,int j,int prev,vector<vector<int>>&dp){
+        
+        if(i>=n || j>=m || i<0 || j<0 || prev >= matrix[i][j])
+            return 0;
+        
+        if(dp[i][j]!=-1)
+            return dp[i][j];
+        
+        int ans = 1 + max({helper(matrix,n,m,i+1,j,matrix[i][j],dp),
+                           helper(matrix,n,m,i,j+1,matrix[i][j],dp),
+                           helper(matrix,n,m,i-1,j,matrix[i][j],dp),
+                           helper(matrix,n,m,i,j-1,matrix[i][j],dp),
+                           });
+        
+        return dp[i][j] = ans;
+    }
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        
+        int ans = 0;
+        
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){ 
+                ans = max(ans,helper(matrix,n,m,i,j,-1,dp));
+            }
+        }
+        
+        return ans;
+        
+    }
 };
