8df373ce17fd046b0088cde7f071102ab676b2fe
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-06-02 14:01:32 +0530

    Time: 7 ms (42.62%), Space: 9 MB (5.02%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        
+        int peak;
+        
+        int n = nums.size();
+        
+        for(int i = 0;i<n-1;i++){
+            
+            if(nums[i]>nums[i+1])
+                return i;
+        }
+        
+        return n-1;
+    }
+};
