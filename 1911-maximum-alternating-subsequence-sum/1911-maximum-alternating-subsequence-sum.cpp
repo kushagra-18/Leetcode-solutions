9aa699b8df1700a55ca981096eb1b154fd3a5a45
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-06-16 01:15:49 +0530

    Time: 839 ms (5.10%), Space: 160.9 MB (15.61%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    long long solve(vector<int>&nums,int idx,bool flag,vector<vector<long long int>>&dp){
+//         base case
+        
+        if(idx==nums.size())
+            return 0;
+        
+//         dp
+        if(dp[idx][flag]!=-1)
+            return dp[idx][flag];
+        
+//         take
+        
+        long long take;
+        if(flag)
+            take = nums[idx] + solve(nums,idx+1,!flag,dp);
+        else
+            take = nums[idx]*(-1) + solve(nums,idx+1,!flag,dp);
+        
+        long long notTake = solve(nums,idx+1,flag,dp);
+        
+        return dp[idx][flag] = max(take,notTake);
+            
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        
+        vector<vector<long long int>> dp(nums.size()+1,vector<long long int> (2,-1));
+        return solve(nums,0,true,dp);
+        
+    }
+};
