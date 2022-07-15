0412758090ab298e052df8a440a7e2e16baa3e8b
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-07-16 02:12:00 +0530

    Time: 4 ms (97.46%), Space: 13.9 MB (90.73%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     
+    int dx[4] = {-1,0,1,0};
+    int dy[4] = {0,1,0,-1};
+    
     void dfs(vector<vector<int>>& image,int i,int j,int n,int m ,int color,int newColor){
         
         if(i>=n || i<0 || j>=m || j<0 || image[i][j]!=color)
@@ -8,10 +11,13 @@ public:
         
         image[i][j] = newColor;
         
-         dfs(image,i+1,j,n,m,color,newColor);
-         dfs(image,i,j-1,n,m,color,newColor);
-         dfs(image,i,j+1,n,m,color,newColor);
-         dfs(image,i-1,j,n,m,color,newColor);
+        for(int x = 0;x<4;x++)
+            dfs(image,i+dx[x],j+dy[x],n,m,color,newColor);
+        
+         // dfs(image,i+1,j,n,m,color,newColor);
+         // dfs(image,i,j-1,n,m,color,newColor);
+         // dfs(image,i,j+1,n,m,color,newColor);
+         // dfs(image,i-1,j,n,m,color,newColor);
         
         
     }
