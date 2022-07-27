be740d15415cd5fac5f623b2c8591302be02f826
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-07-27 15:41:04 +0530

    Time: 23 ms (70.14%), Space: 17.1 MB (14.90%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,54 @@
+class Solution
+{
+    public:
+        int lowerBound(vector<int> arr, int X)
+        {
+            int mid;
+            
+            int N = arr.size();
+
+            int low = 0;
+            int high = N;
+
+            while (low < high)
+            {
+                mid = low + (high - low) / 2;
+
+                if (X <= arr[mid])
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+
+            return low;
+        }
+    int lengthOfLIS(vector<int> &nums)
+    {
+
+        int n = nums.size();
+        vector<int> temp;
+
+        temp.push_back(nums[0]);
+
+        for (int i = 1; i < n; i++)
+        {
+
+            if (nums[i] > temp.back())
+                temp.push_back(nums[i]);
+            else
+            {
+
+                // auto idx = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
+
+                auto idx = lowerBound(temp,nums[i]);
+                temp[idx] = nums[i];
+            }
+        }
+
+        return temp.size();
+    }
+};
