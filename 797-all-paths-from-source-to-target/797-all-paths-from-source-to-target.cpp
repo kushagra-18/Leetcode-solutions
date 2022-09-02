2d6a9c0107000fbf65eb805985b814b37151ba2b
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-09-02 23:54:52 +0530

    Time: 18 ms (73.09%), Space: 12.1 MB (51.97%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,33 +1,32 @@
-class Solution {
-public:
-    
-    vector<int> path;
-    vector<vector<int>> res;
-   void dfs(vector<vector<int>>& graph,int curr,int t){
-       
-       path.push_back(curr);
-       
-       if(curr == t)
-           res.push_back(path);
-       else{
-           
-           
-           for(auto u: graph[curr])
-               dfs(graph,u,t);
-           
-       }
-       
-       path.pop_back();
-       
-       
-   }
-    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        
-        int t = graph.size()-1;
-        
-        dfs(graph,0,t);
-        
+class Solution
+{
+    public:
+
+        vector<int> path;
+    vector<vector < int>> res;
+    void dfs(vector<vector < int>> &graph, int curr, int t)
+    {
+
+        path.push_back(curr);
+
+        if (curr == t)
+            res.push_back(path);
+        else
+        {
+
+            for (auto u: graph[curr])
+                dfs(graph, u, t);
+        }
+
+        path.pop_back();
+    }
+    vector<vector < int>> allPathsSourceTarget(vector<vector < int>> &graph)
+    {
+
+        int t = graph.size() - 1;
+
+        dfs(graph, 0, t);
+
         return res;
-        
     }
 };
