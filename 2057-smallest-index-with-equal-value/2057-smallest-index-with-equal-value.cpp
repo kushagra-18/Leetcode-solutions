fdc5400215d2febb10ba5c60081bc6be0d3a0dda
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-11-10 01:13:33 +0530

    Time: 23 ms (62.34%), Space: 22.1 MB (34.93%) - LeetHub

--- 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.cpp
+++ 2057-smallest-index-with-equal-value/2057-smallest-index-with-equal-value.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
-  int smallestEqual(vector<int>& nums) {
-    for (int i = 0; i < nums.size(); ++i)
-        if (nums[i] == i % 10)
-            return i;
-    return -1;
-}
+int smallestEqual(vector<int>& nums) {
+        
+          for(int i=0 ; i< nums.size() ; i++)
+          {
+              if(nums[i] >= 10 ) continue;
+              if(nums[i] == i%10)
+                  return i ;
+          }
+          return -1 ;
+    } 
 };
