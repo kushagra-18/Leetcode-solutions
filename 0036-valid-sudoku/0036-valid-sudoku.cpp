472bdfda18ce5a0eb8eed4d3f477e55c4e2eebbf
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-11-19 01:24:17 +0530

    Time: 177 ms (5.11%), Space: 18.1 MB (62.78%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    bool isValid(vector<vector<char>>& board, int row,int col,char c){
+        
+        for(int i = 0;i<9;i++){
+            
+            string temp = to_string(row) + ',' + to_string(col);
+            string temp1 = to_string(row) + ',' + to_string(i);
+            
+           if(temp==temp1)
+               continue;
+            
+            if(board[row][i]==c){
+                cout<<'a';
+                return false;
+            }
+            
+            temp1 = to_string(i) + ',' + to_string(col);
+            
+            if(temp==temp1)
+                continue;
+
+            if(board[i][col]==c){
+                 cout<<'b';
+                return false;
+            }
+            
+            temp1 = to_string(3*(row/3) + i/3) + ',' + to_string(3*(col/3) + i%3);
+             
+            if(temp==temp1)
+                continue;
+
+            
+            if(board[3*(row/3) + i/3][3*(col/3) + i%3]==c){
+                cout<<'c';
+                return false;
+            }
+
+        }
+
+        return true;
+        
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+      int n = board.size();
+        int m = board[0].size();
+        
+        for(int i = 0;i<n;i++){
+            
+            for(int j = 0;j<m;j++){
+                
+                if(board[i][j]!='.'){
+
+                    char c = board[i][j];
+
+                    if(!isValid(board,i,j,c))
+                        return false;
+                    
+                }
+                
+            }
+            
+        }
+        
+        return true;
+    }
+};
