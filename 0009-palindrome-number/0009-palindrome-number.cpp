55c62c4e8fd6a2a48af9a752be83f9b164b8353a
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-11-25 23:59:06 +0530

    Time: 18 ms (80.82%), Space: 5.9 MB (32.69%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(int num) {
+        
+        
+        if(num<0)
+            return false;
+        
+        long int y,n = 0,x = 0;
+        
+         x = num;
+        while(x!=0){
+            
+            y = x%10;
+            n = n*10 + y;
+            x = x/10;
+            
+            
+        }
+        
+    
+        return (n == num)?true: false;
+            
+    }
+};
