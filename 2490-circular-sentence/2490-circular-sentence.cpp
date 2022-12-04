aae08ec56d44c888c3a6ee9e60442d56ee25bc50
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-12-04 13:45:24 +0530

    Time: 3 ms (100.00%), Space: 6.7 MB (80.00%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isCircularSentence(string s) {
+        
+        int n = s.size();
+        
+        if(s[0]!=s[n-1])
+            return false;
+        
+        // char prev;
+        // char nxt;
+        
+        for(int i = 0;i<n-1;i++){
+            
+            if(s[i]==' ')
+            {
+                if(s[i-1]!=s[i+1])
+                    return false;
+            }
+            
+        }
+        
+        return true;
+    }
+};
