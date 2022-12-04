6793b3e934d730355a4a21684755747f88691358
Kushagra Sharma <46473446+kushagra-18@users.noreply.github.com>
2022-12-04 20:13:57 +0530

    Time: 236 ms (33.33%), Space: 52.4 MB (8.33%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -4,23 +4,30 @@ public:
         
         sort(skill.begin(),skill.end());
         
-        unordered_set<int> st;
+        // unordered_set<int> st;
         
         long long sum = 0;
         
         int j = skill.size()-1;
         int i = 0;
         
-        int skillTot;
+        int skillTot = -1;
         
         while(i<j){
             
+            if(skillTot!=-1){
+                
+                if(skillTot!=skill[i]+skill[j])
+                    return -1;
+                
+            }
+            
             skillTot = skill[i]+skill[j];
             
-            st.insert(skillTot);
+//             st.insert(skillTot);
             
-            if(st.size()>1)
-                return -1;
+//             if(st.size()>1)
+//                 return -1;
             
             sum+= skill[i]*skill[j];
 
